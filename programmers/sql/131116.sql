a27eead50c5725334d281ec8a420059c6eb673d9
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-02 00:34:07 +0900

    식품분류별 가장 비싼 식품의 정보 조회하기

---
+++ programmers/sql/131116.sql
@@ -0,0 +1,8 @@
+-- 식품분류별 가장 비싼 식품의 정보 조회하기
+select category, price, product_name
+from food_product
+where category in ('과자', '국', '김치', '식용유')
+      and price in (select max(price) 
+                    from food_product
+                    group by category)
+order by price desc
