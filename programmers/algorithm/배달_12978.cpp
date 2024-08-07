41969552aa1a72952f3f91cac4e42fa7363882e1
Lim jeong woo <dlawjddn0105@gmail.com>
2024-08-07 15:20:22 +0900

    배달

---
+++ programmers/algorithm/배달_12978.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file 배달_12978.cpp
+ * @brief 프로그래머스 lv.2 Summer/Winter Coding(~2018) 배달
+ * @version 0.1
+ * @date 2024-08-07
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define INF 1e15
+
+using namespace std;
+
+vector<vector<pair<int, int> > > maps;
+vector<long long> visited;
+
+int dijkstra(int k) {
+    priority_queue<pair<long long, int>, vector<pair<long long, int> >, greater<pair<long long, int> > > pq;
+    pq.push({0, 1});
+    visited[1] = 0;
+    while(!pq.empty()) {
+        auto[cost, pos] = pq.top(); pq.pop();
+        if (visited[pos] != cost) continue;
+        for(int x=0; x<maps[pos].size(); x++) {
+            int npos = maps[pos][x].first;
+            int ncost = cost + maps[pos][x].second;
+            if (visited[npos] <= ncost) continue;
+            pq.push({ncost, npos});
+            visited[npos] = ncost;
+        }
+    }
+    int cnt = 0;
+    for(int i=1; i<visited.size(); i++) {
+        if (visited[i] <= k) cnt++;
+    }
+    return cnt;
+}
+
+int solution(int N, vector<vector<int> > roads, int K) {
+    maps.resize(N+1);
+    visited.resize(N+1, INF);
+    for(auto road : roads) {
+        int node1 = road[0];
+        int node2 = road[1];
+        int cost = road[2];
+        maps[node1].push_back({node2, cost});
+        maps[node2].push_back({node1, cost});
+    }
+    return dijkstra(K);
+}
