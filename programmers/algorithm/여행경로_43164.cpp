dfb1606dce974278ed43331cab6fad09906f549d
Lim jeong woo <dlawjddn0105@gmail.com>
2024-08-18 19:07:03 +0900

    여행 경로 2트

--- programmers/algorithm/여행경로_43164.cpp
+++ programmers/algorithm/여행경로_43164.cpp
@@ -30,3 +30,46 @@ vector<string> solution(vector<vector<string>> tickets) {
     }
     return answer;
 }
+/**
+ * @file 여행경로_43164.cpp
+ * @brief 프로그래머스 lv.3 여행경로
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> answer;
+vector<bool> visited;
+
+void dfs(vector<string> path, vector<vector<string> >& tickets) {
+    if (path.size() == tickets.size() + 1) {
+        if (answer.size() == 0) {
+            answer = path;
+            return ;
+        }
+        answer = min(answer, path);
+        return ;
+    }
+    string pos = path.back();
+    for(int i=0; i<tickets.size(); i++) {
+        if (visited[i] || pos != tickets[i][0]) continue;
+        visited[i] = true;
+        path.push_back(tickets[i][1]);
+        dfs(path, tickets);
+        path.pop_back();
+        visited[i] = false;
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    visited.resize(tickets.size(), false);
+    vector<string> path = {"ICN"};
+    dfs(path, tickets);
+    return answer;
+}
