5704031a59d7fda53287680b0fe270a105e28abb
Lim jeong woo <dlawjddn0105@gmail.com>
2024-08-20 19:00:49 +0900

    가장 긴 팰린드롬 2트

--- programmers/algorithm/가장긴팰린드롬_12904.cpp
+++ programmers/algorithm/가장긴팰린드롬_12904.cpp
@@ -48,3 +48,50 @@ int solution(string s){
     }
     return answer;
 }
+/**
+ * @file 가장긴팰린드롬_12904.cpp
+ * @brief 프로그래머스 lv.3 가장 긴 팰린드롬
+ * @version 0.1
+ * @date 2024-08-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool check_outside(int left, int right, int length) {
+    return left < 0 || left >= length || right < 0 || right >= length;
+}
+
+int solution(string s)
+{
+    int answer = 0;
+    // 팰린드롬이 홀수 길이인 경우
+    for(int i=0; i<s.size(); i++) {
+        int left = i - 1;
+        int right = i + 1;
+        int cnt = 0;
+        while(!check_outside(left, right, s.size()) && s[left] == s[right]) {
+            cnt++;
+            left--;
+            right++;
+        }
+        answer = max(answer, 2 * cnt + 1);
+    }
+    // 팰린드롬이 짝수 길이인 경우
+    for(int i=0; i<s.size(); i++) {
+        int left = i;
+        int right = i + 1;
+        int cnt = 0;
+        while(!check_outside(left, right, s.size()) && s[left] == s[right]) {
+            cnt++;
+            left--;
+            right++;
+        }
+        answer = max(answer, 2 * cnt);
+    }
+    return answer;
+}
