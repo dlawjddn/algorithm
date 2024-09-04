d4dc1582376bcf00ce7fcba8443bece9a1372360
Lim jeong woo <dlawjddn0105@gmail.com>
2024-09-05 01:42:43 +0900

    무인도 여행

--- programmers/algorithm/무인도여행_154540.cpp
+++ programmers/algorithm/무인도여행_154540.cpp
@@ -59,3 +59,62 @@ vector<int> solution(vector<string> maps) {
         sort(answer.begin(), answer.end());
     return answer;
 }
+/**
+ * @file 무인도여행_154540.cpp
+ * @brief 프로그래머스 lv.2 무인도 여행
+ * @version 0.1
+ * @date 2024-09-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int moveY[4]={0,1,0,-1}, moveX[4]={1,0,-1,0};
+vector<vector<bool> > visited;
+
+bool check_outside(int y, int x) {
+    return y < 0 || y >= visited.size() || x < 0 || x >= visited[y].size();
+}
+
+int bfs(int sy, int sx, vector<string>& maps) {
+    int cnt = maps[sy][sx] - '0';
+    queue<pair<int, int> > q;
+    q.push({sy, sx});
+    visited[sy][sx] = true;
+    while(!q.empty()) {
+        auto[y, x] = q.front(); q.pop();
+        for(int d=0; d<4; d++) {
+            int ny = y + moveY[d];
+            int nx = x + moveX[d];
+            if (check_outside(ny, nx) || visited[ny][nx] || maps[ny][nx] == 'X') continue;
+            q.push({ny, nx});
+            visited[ny][nx] = true;
+            cnt += maps[ny][nx] - '0';
+        }
+    }
+    return cnt;
+}
+
+vector<int> solve(vector<string>& maps) {
+    vector<int> result;
+    for(int y=0; y<maps.size(); y++) {
+        for(int x=0; x<maps[y].size(); x++) {
+            if (visited[y][x] || maps[y][x] == 'X') continue;
+            result.push_back(bfs(y, x, maps));
+        }
+    }
+    if (result.empty()) return {-1};
+    sort(result.begin(), result.end());
+    return result;
+}
+
+vector<int> solution(vector<string> maps) {
+    visited.resize(maps.size(), vector<bool>(maps[0].size(), false));
+    return solve(maps);
+}
