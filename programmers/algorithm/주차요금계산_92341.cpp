62ff20726a9ccff534f0ae9ff1c528e609a5686c
Lim jeong woo <dlawjddn0105@gmail.com>
2024-09-05 14:45:09 +0900

    주차 요금 계산

--- programmers/algorithm/주차요금계산_92341.cpp
+++ programmers/algorithm/주차요금계산_92341.cpp
@@ -89,3 +89,85 @@ vector<int> solution(vector<int> fees, vector<string> records) {
     
     return answer;
 }
+
+/**
+ * @file 주차요금계산_92341.cpp
+ * @brief 프로그래머스 lv.2 카카오 2022 공채 주차요금계산
+ * @version 0.1
+ * @date 2024-09-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+unordered_map<string, int> total_time;
+unordered_map<string, pair<int, int> > in_out;
+
+vector<string> split(string origin, char delimeter) {
+    vector<string> result;
+    string token;
+    istringstream token_stream(origin);
+    while(getline(token_stream, token, delimeter)) {
+        result.push_back(token);
+    }
+    return result;
+}
+
+int change_to_int(string time) {
+    vector<string> splited = split(time, ':');
+    int hour = stoi(splited[0]);
+    int minute = stoi(splited[1]);
+    return hour * 60 + minute;
+}
+
+int cal_fee(vector<int>& fees, int total_minute) {
+    int basic_minute = fees[0];
+    int basic_fee = fees[1];
+    int over_minute = fees[2];
+    int over_fee = fees[3];
+    if (total_minute <= basic_minute) {
+        return basic_fee;
+    }
+    else {
+        int over_time = total_minute - basic_minute;
+        if (over_time % over_minute == 0) 
+            return basic_fee + over_fee * (over_time / over_minute);
+        else return basic_fee + over_fee * ((over_time / over_minute) + 1);
+    }
+}
+
+vector<int> solution(vector<int> fees, vector<string> records) {
+    vector<int> answer;
+    for(auto record : records) {
+        vector<string> info = split(record, ' ');
+        if (info[2] == "IN") {
+            in_out[info[1]] = {change_to_int(info[0]), 23 * 60 + 59};
+        }
+        else {
+            int come_time = in_out[info[1]].first;
+            int out_time = change_to_int(info[0]);
+            total_time[info[1]] += (out_time - come_time);
+            in_out.erase(info[1]);
+        }
+    }
+    for(auto record : in_out) {
+        string car_id = record.first;
+        pair<int, int> info = record.second;
+        total_time[car_id] += (info.second - info.first);
+    }
+    set<pair<string, int> > result(total_time.begin(), total_time.end());
+    for(auto r : result) {
+        answer.push_back(cal_fee(fees, r.second));
+    }
+    return answer;
+}
