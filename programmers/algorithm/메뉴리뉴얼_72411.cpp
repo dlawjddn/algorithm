ddf5430d0b7d662a6ea6199136cbff9d8fdff96a
Lim jeong woo <dlawjddn0105@gmail.com>
2024-09-27 20:19:03 +0900

    메뉴 리뉴얼

--- programmers/algorithm/메뉴리뉴얼_72411.cpp
+++ programmers/algorithm/메뉴리뉴얼_72411.cpp
@@ -56,3 +56,58 @@ vector<string> solution(vector<string> orders, vector<int> course) {
     sort(answer.begin(), answer.end());
     return answer;
 }
+/**
+ * @file 메뉴리뉴얼_72411.cpp
+ * @brief 프로그래머스 lv.2 2021 카카오 공채 메뉴 리뉴얼
+ * @version 0.1
+ * @date 2024-09-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> favorite_cnt;
+vector<bool> visited;
+unordered_map<string, int> order_cnt;
+
+void combination(string menus, int limit, int idx, int cnt, string choose) {
+    if (cnt == limit) {
+        sort(choose.begin(), choose.end());
+        order_cnt[choose] += 1;
+        favorite_cnt[choose.size()] = max(favorite_cnt[choose.size()], order_cnt[choose]);
+        return ;
+    }
+    for(int i=idx; i<menus.size(); i++) {
+        if (visited[i]) continue;
+        visited[i] = true;
+        combination(menus, limit, i, cnt + 1, choose + menus[i]);
+        visited[i] = false;
+    }
+}
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    favorite_cnt.resize(11, 0);
+    for(auto order : orders) {
+        for(auto cnt : course) {
+            visited = vector<bool>(order.size(), false);
+            combination(order, cnt, 0, 0, "");
+        }
+    }
+    for(int i=0; i<course.size(); i++) {
+        int len = favorite_cnt[course[i]];
+        for(auto order : order_cnt) {
+            if (order.second < 2 || order.second != len || order.first.size() != course[i]) continue;
+            answer.push_back(order.first);
+        }
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
