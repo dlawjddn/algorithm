0dd790ac703eb873b9b62e5e78863889f7281f43
Lim jeong woo <dlawjddn0105@gmail.com>
2024-09-27 17:29:02 +0900

    오픈 채팅방

--- programmers/algorithm/오픈채팅방_42888.cpp
+++ programmers/algorithm/오픈채팅방_42888.cpp
@@ -58,3 +58,69 @@ vector<string> solution(vector<string> record) {
     }
     return answer;
 }
+/**
+ * @file 오픈채팅방_42888.cpp
+ * @brief 프로그래머스 lv.2 2019 카카오 공채 오픈 채팅방
+ * @version 0.1
+ * @date 2024-09-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ * 이번에는 한번에 풀었지롱~
+ * 
+ */
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+struct Info {
+    string action, userid, nickname;
+};
+unordered_map<string, string> user_name;
+
+Info make_info(string action, string userid, string nickname) {
+    Info info;
+    info.action = action;
+    info.userid = userid;
+    info.nickname = nickname;
+    return info;
+}
+
+vector<string> split(string origin) {
+    vector<string> result;
+    string token;
+    istringstream token_stream(origin);
+    while(getline(token_stream, token, ' ')) {
+        result.push_back(token);
+    }
+    return result;
+}
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    vector<Info> data;
+    for(auto r : record) {
+        vector<string> splited = split(r);
+        string action = splited[0];
+        string userid = splited[1];
+        string nickname = "";
+        if (splited[0] == "Enter" || splited[0] == "Change") {
+            nickname = splited[2];
+            user_name[userid] = nickname;
+        }
+        data.push_back(make_info(action, userid, nickname));
+    }
+    for(auto user : data) {
+        if (user.action == "Change") continue;
+        
+        string str_answer = user_name[user.userid];
+        if (user.action == "Enter") str_answer += "님이 들어왔습니다.";
+        else str_answer += "님이 나갔습니다.";
+        answer.push_back(str_answer);
+    }
+    return answer;
+}
