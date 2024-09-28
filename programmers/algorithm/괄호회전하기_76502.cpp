e05c9e1eb98609be71747e6adf9a1b80b27a7585
Lim jeong woo <dlawjddn0105@gmail.com>
2024-09-28 17:57:37 +0900

    괄호 회전하기

--- programmers/algorithm/괄호회전하기_76502.cpp
+++ programmers/algorithm/괄호회전하기_76502.cpp
@@ -40,3 +40,44 @@ int solve(string brakets){
 int solution(string s) {
     return solve(s);
 }
+/**
+ * @file 괄호회전하기_76502.cpp
+ * @brief 프로그래머스 lv.2 괄호 회전하기
+ * @version 0.1
+ * @date 2024-09-28
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+bool check_correct(string str) {
+    stack<char> s;
+    for(auto c : str) {
+        if (s.empty()) {
+            s.push(c);
+            continue;
+        }
+        if (s.top() == '{' && c == '}') s.pop();
+        else if (s.top() == '[' && c == ']') s.pop();
+        else if (s.top() == '(' && c == ')') s.pop();
+        else s.push(c);
+    }
+    if (s.empty()) return true;
+    return false;
+}
+
+int solution(string s) {
+    int answer = 0;
+    s += s;
+    int limit = s.size() / 2;
+    for(int start = 0; start<limit; start++) {
+        if (check_correct(s.substr(start, limit))) answer++;
+    }
+    return answer;
+}
