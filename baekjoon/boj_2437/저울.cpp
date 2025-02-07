5c62885f8a598e517d22d7b615e8022f0fdd46bc
Lim jeong woo <dlawjddn0105@gmail.com>
2025-02-07 15:34:08 +0900

    저울

--- baekjoon/boj_2437/저울.cpp
+++ baekjoon/boj_2437/저울.cpp
@@ -1,53 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
 int main() {
-    int num_cnt;
-    cin >> num_cnt;
-    vector<int> numbers(num_cnt);
-    
-    for (int i = 0; i < num_cnt; i++) {
-        cin >> numbers[i];
+    int num_cnt; cin>>num_cnt;
+    vector<int> numbers;
+    for(int i=0; i<num_cnt; i++) {
+        int num; cin>>num;
+        numbers.push_back(num);
     }
     sort(numbers.begin(), numbers.end());
 
-    set<int> unique_numbers;
     int answer = 1;
-    
-    if (answer != numbers.front()) {
-        cout << answer;
-        return 0;
-    } else {
-        answer++;
-        unique_numbers.insert(numbers.front());
-    }
 
-    for (int i = 1; i < numbers.size(); i++) {
-        if (answer >= numbers[i]) {
-            if (answer == numbers[i]) unique_numbers.insert(answer++);
-        } else {
-            cout << answer;
-            return 0;
+    for(int i=0; i<numbers.size(); i++) {
+        if (answer < numbers[i]) {
+            //cout<<answer;
+            break;
         }
-        
-        vector<int> to_add;  // 새로운 값 저장할 벡터
-        for (auto before_value : unique_numbers) {
-            int now_value = before_value + numbers[i];
-            if (answer >= now_value) {
-                if (answer == now_value) to_add.push_back(answer++);
-            } else {
-                cout << answer;
-                return 0;
-            }
-        }
-        
-        for (auto value : to_add) unique_numbers.insert(value);  // 한 번에 추가
+        answer += numbers[i];
     }
-
-    cout << answer;  // 최종 정답 출력
-    return 0;
+    cout<<answer;
 }
