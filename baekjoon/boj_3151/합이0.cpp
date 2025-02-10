51da7cc0572897b54fee50c653e6e3cd2f2fea30
Lim jeong woo <dlawjddn0105@gmail.com>
2025-02-10 16:28:47 +0900

    합이 0

--- baekjoon/boj_3151/합이0.cpp
+++ baekjoon/boj_3151/합이0.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int num_cnt; cin>>num_cnt;
     vector<int> numbers;
     for(int i=0; i<num_cnt; i++) {
@@ -14,7 +15,7 @@ int main() {
     sort(numbers.begin(), numbers.end());
 
     long long answer = 0;
-    for(int left = 0; left < numbers.size() - 2; left++) {
+    for(int left = 0; left < numbers.size() - 1; left++) {
         for(int right = left + 1; right < numbers.size(); right++) {
             int left_num = numbers[left];
             int right_num = numbers[right];
