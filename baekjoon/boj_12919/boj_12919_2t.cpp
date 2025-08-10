3a2364f3a4c88d97b24c7a3f6f3cc1c0f216c3f1
Lim jeong woo <dlawjddn0105@gmail.com>
2025-08-10 23:46:06 +0900

    A와 B 2

---
+++ baekjoon/boj_12919/boj_12919_2t.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+queue<string> q;
+
+int main() {
+    bool findout = false;
+    string s, t; cin>>s>>t;
+    q.push(t);
+    while(!q.empty()) {
+        string now = q.front(); q.pop();
+
+        if (now == s) {
+            findout = true;
+            break;
+        }
+
+        if (now.back() == 'A') {
+            string next = now.substr(0, now.size() - 1);
+            if (next.size() < s.size()) continue;
+            q.push(next);
+        }
+
+        if (now.front() == 'B') {
+            string next = now.substr(1, now.size());
+            reverse(next.begin(), next.end());
+            if (next.size() < s.size()) continue;
+            q.push(next);
+        }
+    }
+    cout<<((findout) ? 1 : 0);
+}
